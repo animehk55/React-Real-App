c564288525c349736bc1f8260aa86bb21cffe908
Animesh <animeshk55@gmail.com>
2018-09-06 17:11:57 +0530

    Development Server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
-import product from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="box">
+         <header className="box">
+           <h1 className="App-title">Welcome to React</h1>
+         </header>
+         <p className="App-intro">
+           To get started, edit <code>src/App.js</code> and save to reload.
+         </p>
+       </div>
     );
   }
 }
 
 export default App;
-
